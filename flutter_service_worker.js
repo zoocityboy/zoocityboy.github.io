eb68226df256ddebfbdad5e3404a2b12874cab82
zoocityboy <zoocityboy@gmail.com>
2022-06-21 10:00:09 +0200

    v1.0.1

---
+++ flutter_service_worker.js
@@ -0,0 +1,201 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "favicon-16x16.png": "94a6aefdac646ed80049c5732f88f784",
+"version.json": "9007070bdda28f1d05c1b48020783fc9",
+"index.html": "9b443034368b3b1eaa9e9edd0fc69cfe",
+"/": "9b443034368b3b1eaa9e9edd0fc69cfe",
+"main.dart.js": "d99934621340eb327a0a867c987a4ce3",
+"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
+"favicon-96x96.png": "568315d5c96e254abecc1bd6fa6d579b",
+"icons/Icon-192.png": "76148aceb4addd2cd0cd6c182c4be453",
+"icons/Icon-maskable-192.png": "76148aceb4addd2cd0cd6c182c4be453",
+"icons/Icon-maskable-512.png": "9673b9b9c1ce6cad02298a2e987e17e0",
+"icons/Icon-512.png": "9673b9b9c1ce6cad02298a2e987e17e0",
+"manifest.json": "32a8c2436015dc7e16ef2832af1ef1aa",
+"assets/res/zoocityboy_light.png": "ca66108d422a121c980df0be8c5d34a1",
+"assets/res/zoocityboy_dark.png": "9639ecae36a29237890522c54db90121",
+"assets/AssetManifest.json": "94785b7dfacfeb8da2955245352874ed",
+"assets/NOTICES": "0a3edd12cc029644682adbb0aa5d8567",
+"assets/FontManifest.json": "3ca64090b9da4a7c0a3988ea3cda93fc",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/packages/ionicons/fonts/Ionicons.ttf": "0cdf2a324d5c21f08c7f446476aa2ee3",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"favicon-32x32.png": "d446f0955f600d9bbc54d9d621b2d4e5",
+"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
+"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
+"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
+"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
+};
+
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = [
+  "main.dart.js",
+"index.html",
+"assets/NOTICES",
+"assets/AssetManifest.json",
+"assets/FontManifest.json"];
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache.
+        return response || fetch(event.request).then((response) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
--- zoocityboy_web/lib/presentation/main_screen.dart
+++ zoocityboy_web/lib/presentation/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -10,30 +12,103 @@ class MainScreen extends StatelessWidget {
         return SizedBox(
           width: constraints.maxWidth,
           height: constraints.maxHeight,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'res/zoocityboy_light.png',
-                  height: 96,
-                  cacheHeight:
-                      (MediaQuery.of(context).devicePixelRatio * 96).toInt(),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 64),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Image.asset(
+                            'res/zoocityboy_light.png',
+                            height: 96,
+                            cacheHeight:
+                                (MediaQuery.of(context).devicePixelRatio * 96)
+                                    .toInt(),
+                          ),
+                          Text(
+                            'Dart & Flutter developer',
+                            style: Theme.of(context).textTheme.titleLarge,
+                          ),
+                        ],
+                      ),
+                    ),
+                    // Text(
+                    //   'Hi there, I\'m 🦏 zoocityboy',
+                    //   style: Theme.of(context).textTheme.titleMedium,
+                    // ),
+                    // Text(
+                    //   ' 🔭 I’m currently working on ... HCI SuperCupo app and a lot of private Flutter/Dart projects',
+                    //   style: Theme.of(context).textTheme.bodySmall,
+                    // ),
+                    // Text(
+                    //   '👯 I’m looking to collaborate on ... Supabase & Flutter projects',
+                    //   style: Theme.of(context).textTheme.bodySmall,
+                    // ),
+                    // Text(
+                    //   '🧘🏻‍♂️ I’m focusing on ... Developer experience on huge Flutter projects (author of VSCode extensions, dart cli apps).',
+                    //   style: Theme.of(context).textTheme.bodySmall,
+                    // ),
+                    // Text(
+                    //   '⚡ Fun fact: My little son knows, what a pipeline is!',
+                    //   style: Theme.of(context).textTheme.bodySmall,
+                    // ),
+                    const SizedBox(
+                      height: 96,
+                    ),
+                    Wrap(
+                      spacing: 8,
+                      runSpacing: 8,
+                      runAlignment: WrapAlignment.spaceEvenly,
+                      children: [
+                        TextButton.icon(
+                            onPressed: _launchGithubUrl,
+                            icon: const Icon(Ionicons.logo_github),
+                            label: const Text('Github')),
+                      ],
+                    ),
+                    TextButton.icon(
+                        onPressed: _launchTwitterUrl,
+                        icon: const Icon(Ionicons.logo_twitter),
+                        label: const Text('Twitter')),
+                  ],
                 ),
-                const SizedBox(height: 16),
-                Text(
-                  'Fluter & dart for web',
-                  style: Theme.of(context).textTheme.titleLarge,
+              ),
+              Positioned(
+                bottom: 8,
+                left: 8,
+                right: 8,
+                height: 32,
+                child: Center(
+                  child: Text(
+                    'Developed by 🦏 @zoocityboy',
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
                 ),
-                Text(
-                  'Developed by 🦏 @zoocityboy',
-                  style: Theme.of(context).textTheme.bodySmall,
-                )
-              ],
-            ),
+              )
+            ],
           ),
         );
       }),
     );
+  }
+
+  void _launchGithubUrl() async {
+    final url = Uri.parse('https://github.com/zoocityboy');
+    if (!await launchUrl(url)) {
+      print('Could not launch $url');
+    }
+  }
+
+  void _launchTwitterUrl() async {
+    final url = Uri.parse('https://twitter.com/don235078');
+    if (!await launchUrl(url)) {
+      print('Could not launch $url');
+    }
   }
 }
